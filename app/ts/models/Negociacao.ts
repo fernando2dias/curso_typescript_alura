0a6c39ea83f1c22ee18fb63853ee60272aaad728
Fernando Dias Motta <180016@facens.br>
2020-06-09 15:05:10 -0300

    aula 02

--- app/ts/models/Negociacao.ts
+++ app/ts/models/Negociacao.ts
@@ -1,26 +1,7 @@
 class Negociacao{
 
-    private _data;
-    private _quantidade;
-    private _valor;
-
-    constructor(data, quantidade, valor){
-        this._data = data;
-        this._quantidade = quantidade;
-        this._valor = valor;
-
-        if(!data){
-            throw new Error('Data deve ser preenchida');
-        }
-
-        if(!quantidade){
-            throw new Error('Quantidade deve ser preenchida');
-        }
-
-        if(!valor){
-            throw new Error('Valor deve ser preenchida');
-        }
-
+       constructor(private _data : Date, private _quantidade : number, private _valor : number){
+        
     }
 
     get data(){
