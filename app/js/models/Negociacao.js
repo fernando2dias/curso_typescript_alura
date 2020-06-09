8192c19041f65c8355cffae122a9b33648a4797d
Fernando Dias Motta <180016@facens.br>
2020-06-09 10:34:31 -0300

    primeira aula

---
+++ app/js/models/Negociacao.js
@@ -0,0 +1,28 @@
+class Negociacao {
+    constructor(data, quantidade, valor) {
+        this._data = data;
+        this._quantidade = quantidade;
+        this._valor = valor;
+        if (!data) {
+            throw new Error('Data deve ser preenchida');
+        }
+        if (!quantidade) {
+            throw new Error('Quantidade deve ser preenchida');
+        }
+        if (!valor) {
+            throw new Error('Valor deve ser preenchida');
+        }
+    }
+    get data() {
+        return this._data;
+    }
+    get quantidade() {
+        return this._quantidade;
+    }
+    get valor() {
+        return this._valor;
+    }
+    get volume() {
+        return this._quantidade * this._valor;
+    }
+}
